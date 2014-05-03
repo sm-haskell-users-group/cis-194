ad5933a9edff90ac797f96ea9531e2c03feec65b
Devon Stewart <blast@hardchee.se>
2014-05-02 22:43:09 -0700

    w01e01: toDigits implementation

--- src/Cis194/Hw/Week1.hs
+++ src/Cis194/Hw/Week1.hs
@@ -5,10 +5,12 @@ module Cis194.Hw.Week1 where
 -------------
 
 toDigits :: Integer -> [Integer]
-toDigits x = [x]
+toDigits x
+    | x <= 0    = []
+    | otherwise = toDigits (x `div` 10) ++ [x `mod` 10]
 
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev x = [x]
+toDigitsRev x = reverse (toDigits x)
 
 doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther xs = xs
