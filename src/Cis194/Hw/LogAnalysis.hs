720cb9deed52c31abea1890d32e3aaf108c35cab
Devon Stewart <blast@hardchee.se>
2014-05-31 00:24:50 -0700

    After learning about $, extractInt was unnecessary
    Much cleaner to just `... $ read ...`

--- src/Cis194/Hw/LogAnalysis.hs
+++ src/Cis194/Hw/LogAnalysis.hs
@@ -5,25 +5,22 @@ module Cis194.Hw.LogAnalysis where
 -- Prelude> :set -isrc
 import  Cis194.Hw.Log
 
-extractInt :: [String] -> Int -> Int
-extractInt xs i = read (xs !! i) :: Int
-
 parseMessage :: String -> LogMessage
 
 parseMessage ('W' : ' ' : line) = LogMessage Info timestamp message
     where splitLine = words line
-          timestamp = extractInt splitLine 0
+          timestamp = read $ splitLine !! 0
           message = unwords $ drop 1 splitLine
 
 parseMessage ('I' : ' ' : line) = LogMessage Info timestamp message
     where splitLine = words line
-          timestamp = extractInt splitLine 0
+          timestamp = read $ splitLine !! 0
           message = unwords $ drop 1 splitLine
 
 parseMessage ('E' : ' ' : line) = LogMessage (Error errorCode) timestamp message
     where splitLine = words line
-          timestamp = extractInt splitLine 1
-          errorCode = extractInt splitLine 0
+          timestamp = read $ splitLine !! 1
+          errorCode = read $ splitLine !! 0
           message = unwords $ drop 2 splitLine
 
 parseMessage s = Unknown s
