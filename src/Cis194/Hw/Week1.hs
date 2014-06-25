02ac63d416cd13121591492a7daa88ef15becee1
Stacey Touset <capicue@gmail.com>
2014-06-25 11:41:30 -0700

    Week 1

--- src/Cis194/Hw/Week1.hs
+++ src/Cis194/Hw/Week1.hs
@@ -5,19 +5,28 @@ module Cis194.Hw.Week1 where
 -------------
 
 toDigits :: Integer -> [Integer]
-toDigits x = [x]
+toDigits = reverse . toDigitsRev
 
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev x = [x]
+toDigitsRev n
+  | n <= 0    = []
+  | n < 10    = [n]
+  | otherwise = (n `mod` 10) : toDigitsRev (n `div` 10)
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther xs = xs
+doubleEveryOther = reverse . doubleEveryOther' . reverse
+
+doubleEveryOther' :: [Integer] -> [Integer]
+doubleEveryOther' [] = []
+doubleEveryOther' [x] = [x]
+doubleEveryOther' (x:y:zs) = x : (2 * y) : doubleEveryOther' zs
 
 sumDigits :: [Integer] -> Integer
-sumDigits _ = 0
+sumDigits [] = 0
+sumDigits (x:xs) = sum (toDigits x) + sumDigits xs
 
 validate :: Integer -> Bool
-validate _ = False
+validate n = (sumDigits (doubleEveryOther (toDigits n))) `mod` 10 == 0
 
 ---------------------
 -- Towers of Hanoi --
@@ -27,7 +36,6 @@ type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi _ _ _ _ = []
-
-hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
-hanoi4 _ _ _ _ _ = []
+hanoi 0 _ _ _ = []
+hanoi 1 p1 p2 _ = [(p1, p2)]
+hanoi n p1 p2 p3 = (hanoi (n-1) p1 p3 p2) ++ hanoi 1 p1 p2 p3 ++ (hanoi (n-1) p3 p2 p1)
