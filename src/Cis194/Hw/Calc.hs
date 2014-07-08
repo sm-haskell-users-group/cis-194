4c4f528a45bce7b7aecda52feeeeaf605bea283a
Devon Stewart <blast@hardchee.se>
2014-07-08 00:00:46 -0700

    5-1: ExprT -> Integer

--- src/Cis194/Hw/Calc.hs
+++ src/Cis194/Hw/Calc.hs
@@ -3,7 +3,9 @@ module Cis194.Hw.Calc where
 import  Cis194.Hw.ExprT
 
 eval :: ExprT -> Integer
-eval _ = 0
+eval (Lit x) = x
+eval (Mul x y) = (eval x) * (eval y)
+eval (Add x y) = (eval x) + (eval y)
 
 evalStr :: String -> Maybe Integer
 evalStr _ = Nothing
