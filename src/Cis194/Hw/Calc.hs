b42c639af3a653036c39c101e0e3254dbd22de5f
Devon Stewart <blast@hardchee.se>
2014-07-08 00:02:52 -0700

    5-4: Expr {Integer,Bool,MinMax,Mod7}

--- src/Cis194/Hw/Calc.hs
+++ src/Cis194/Hw/Calc.hs
@@ -3,6 +3,9 @@ module Cis194.Hw.Calc where
 import  Cis194.Hw.ExprT
 import  Cis194.Hw.Parser
 
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
 eval :: ExprT -> Integer
 eval (Lit x) = x
 eval (Mul x y) = (eval x) * (eval y)
@@ -20,3 +23,23 @@ instance Expr ExprT where
     lit x = Lit x
     add x y = Add x y
     mul x y = Mul x y
+
+instance Expr Integer where
+    lit x = x
+    add x y = x + y
+    mul x y = x * y
+
+instance Expr Bool where
+    lit x = x > 0
+    add x y = x || y
+    mul x y = x && y
+
+instance Expr MinMax where
+    lit x = MinMax x
+    add (MinMax x) (MinMax y) = MinMax $ max x y
+    mul (MinMax x) (MinMax y) = MinMax $ min x y
+
+instance Expr Mod7 where
+    lit x = Mod7 $ x `mod` 7
+    add (Mod7 x) (Mod7 y) = Mod7 $ (x + y) `mod` 7
+    mul (Mod7 x) (Mod7 y) = Mod7 $ (x * y) `mod` 7
