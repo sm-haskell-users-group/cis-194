021c296ef34e32cf3456892e50fd1fe87870a4fa
Devon Stewart <blast@hardchee.se>
2014-07-07 23:59:27 -0700

    Committing all my week 4 solutions

--- src/Cis194/Hw/Week4.hs
+++ src/Cis194/Hw/Week4.hs
@@ -7,28 +7,36 @@ fun1 (x:xs)
   | otherwise = fun1 xs
 
 fun1' :: [Integer] -> Integer
-fun1' _ = 0
+fun1' = product . (map $ \x -> x - 2) . (filter even)
 
 fun2 :: Integer -> Integer
 fun2 1 = 0
-fun2 n | even n = n + fun2 (n `div` 2)
-  | otherwise = fun2 (3 * n + 1)
+fun2 n | even n = 1 * n + fun2 (n `div` 2)
+    | otherwise = 0 * n + fun2 (3 * n + 1)
 
 fun2' :: Integer -> Integer
-fun2' _ = 0
+fun2' 1 = 0
+fun2' n = if even n then fun2'e n else fun2'o n
+
+fun2'e n = n + fun2' (n `div` 2)
+fun2'o n = fun2' $ 3 * n + 1
 
 data Tree a = Leaf
   | Node Integer (Tree a) a (Tree a)
   deriving (Show, Eq)
 
 foldTree :: [a] -> Tree a
-foldTree _ = Leaf
+foldTree [] = Leaf
+foldTree (x:xs) = Leaf
 
+-- Odd number of True values
 xor :: [Bool] -> Bool
-xor _ = False
+xor = odd . length . filter id
 
 map' :: (a -> b) -> [a] -> [b]
-map' _ _ = []
+-- map' f xs = foldl (\a x -> (f x) : a) [] xs
+-- map' f = foldl (\a x -> (f x) : a) []
+map' = (flip $ foldr . (\f x a -> f x : a)) []
 
 sieveSundaram :: Integer -> [Integer]
 sieveSundaram _ = []
