bfff0e8ab3a90395eb2fc9c387713e390e4e4450
Devon Stewart <blast@hardchee.se>
2014-07-08 00:01:16 -0700

    5-2: String -> Maybe Integer

--- src/Cis194/Hw/Calc.hs
+++ src/Cis194/Hw/Calc.hs
@@ -1,6 +1,7 @@
 module Cis194.Hw.Calc where 
 
 import  Cis194.Hw.ExprT
+import  Cis194.Hw.Parser
 
 eval :: ExprT -> Integer
 eval (Lit x) = x
@@ -8,4 +9,4 @@ eval (Mul x y) = (eval x) * (eval y)
 eval (Add x y) = (eval x) + (eval y)
 
 evalStr :: String -> Maybe Integer
-evalStr _ = Nothing
+evalStr = (fmap eval) . (parseExp Lit Add Mul)
