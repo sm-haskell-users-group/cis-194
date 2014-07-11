1ea45bc70118df6d8c68b4d43df44ddcbefadb16
Stacey Touset <capicue@gmail.com>
2014-07-11 10:35:54 -0700

    Homework 3: Haskell Golf

--- src/Cis194/Hw.hs
+++ src/Cis194/Hw.hs
@@ -1,22 +1,10 @@
 module Cis194.Hw
-    ( toDigits
-    , doubleEveryOther
-    , sumDigits
-    , validate
-    , hanoi
-    , testParse
-    , testWhatWentWrong
-    , MessageType (..)
-    , LogMessage (..)
-    , MessageTree (..)
-    , parseMessage
-    , parse
-    , insert
-    , build
-    , inOrder
-    , whatWentWrong
-    )where
+    ( localMaxima
+    , histogram
+    , skipsN
+    ) where
 
 import Cis194.Hw.Week1
 import Cis194.Hw.Log
 import Cis194.Hw.LogAnalysis
+import Cis194.Hw.Golf
--- src/Cis194/Hw/Golf.hs
+++ src/Cis194/Hw/Golf.hs
@@ -1,10 +1,25 @@
 module Cis194.Hw.Golf where
 
+import Data.List
+
 skips :: [a] -> [[a]]
-skips _ = []
+skips xs = takeWhile (\x -> not (null x)) [(skipsN n xs) | n <- [1..]]
+
+skipsN :: Int -> [a] -> [a]
+skipsN _ [] = []
+skipsN n xs = if (length (snd parts)) >= n
+                then (last (fst parts)) : (skipsN n (snd parts))
+                else if ((length (fst parts)) >= n)
+                    then (last (fst parts)) : []
+                    else []
+    where parts = splitAt n xs
 
 localMaxima :: [Integer] -> [Integer]
+localMaxima (a:b:c:xs) = if ((b > a) && (b > c))
+                         then b : (localMaxima (b:c:xs))
+                         else (localMaxima (b:c:xs))
 localMaxima _ = []
 
 histogram :: [Integer] -> String
-histogram _ = ""
+histogram xs = intercalate "\n" ((map toStar (reverse (transpose (groupBy (==) (sort xs))))) ++ ["==========", "0123456789", ""])
+    where toStar xs = [j | i <- [0..9], let j = (if (elem i xs) then '*' else ' ')]
