e59c7e8a48e56025656cb6601ee717e5f9ffcc56
Ken Shimizu <kenshimizu88@gmail.com>
2014-07-23 02:05:58 -0700

    super ugly foldtree

--- src/Cis194/Hw/Week4.hs
+++ src/Cis194/Hw/Week4.hs
@@ -31,8 +31,17 @@ data Tree a = Leaf
 
 foldTree :: [a] -> Tree a
 foldTree [] = Leaf
---foldTree [x] = Node 0 Leaf x Leaf
---foldTree xs = foldr (flip createTree) Leaf xs
+foldTree [x] = Node 0 Leaf x Leaf
+foldTree xs = foldr (flip createTree) Leaf xs
+
+createTree :: Tree a -> a -> Tree a
+createTree Leaf x = Node 0 Leaf x Leaf
+createTree (Node i Leaf a Leaf) x = Node (i+1) (createTree Leaf a) x Leaf
+createTree (Node i l a Leaf) x = Node i l x (createTree Leaf a)
+createTree (Node i Leaf a r) x = Node i (createTree Leaf a) x r
+createTree (Node i l@(Node il _ _ _) a r@(Node ir _ _ _)) x
+  | il <= ir = Node i (createTree l a) x r
+  | otherwise = Node (il+1) l x (createTree r a)
 
 xor :: [Bool] -> Bool
 xor = odd . foldl (\y z -> if z then y+1 else y) 0
@@ -47,4 +56,4 @@ sieveSundaram :: Integer -> [Integer]
 sieveSundaram = map (\x -> 2*x + 1) . sundaramList
 
 sundaramList :: Integer -> [Integer]
-sundaramList n = [1..n] \\ [i+j+2*i*j | i <- [1..n], j <- [1..n], i <= j && (i + j + 2*i*j) < n && ] n
+sundaramList n = [1..n] \\ [i+j+2*i*j | i <- [1..n], j <- [1..n], i <= j && (i + j + 2*i*j) < n]
