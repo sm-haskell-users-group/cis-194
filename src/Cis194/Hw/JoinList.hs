76a0d81852afefa5fbd4d4de938bbac2f6842347
Devon Stewart <blast@hardchee.se>
2014-08-03 21:53:21 -0700

    w07e02: Implementing indexJ
    mistake in mconcat caused much headache

--- src/Cis194/Hw/JoinList.hs
+++ src/Cis194/Hw/JoinList.hs
@@ -1,16 +1,34 @@
 module JoinList where
 
 import Data.Monoid
+import Sized
 
 data JoinList m a = Empty
         | Single m a
         | Append m (JoinList m a) (JoinList m a)
     deriving (Eq, Show)
 
+jlToList :: (JoinList a b) -> [b]
+jlToList Empty = []
+jlToList (Single _ a) = [a]
+jlToList (Append m l r) = (jlToList l) ++ (jlToList r)
+
 tag :: Monoid m => JoinList m a -> m
 tag Empty = mempty
 tag (Single m _) = m
-tag (Append m l r) = mconcat [m, tag l, tag r]
+tag (Append m _ _) = m
 
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
 (+++) l r = Append (mappend (tag l) (tag r)) l r
+
+indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
+indexJ i jl
+    | i < 0  = Nothing
+    | i >= s = Nothing
+    where s = getSize $ size $ tag jl
+indexJ _ Empty = Nothing
+indexJ 0 (Single _ a) = Just a
+indexJ i (Append _ l r)
+    | i < ls = indexJ i l
+    | otherwise = indexJ (i - ls) r
+    where ls = getSize $ size $ tag l
--- test/Cis194/Hw/JoinListSpec.hs
+++ test/Cis194/Hw/JoinListSpec.hs
@@ -4,8 +4,15 @@ import Test.Hspec
 import Cis194.Hw.Calc
 import Cis194.Hw.ExprT
 import Cis194.Hw.JoinList
+import Sized
 
 import Data.Monoid
+
+(!!?) :: [a] -> Int -> Maybe a
+[] !!? _ = Nothing
+_ !!? i | i < 0 = Nothing
+(x:xs) !!? 0 = Just x
+(x:xs) !!? i = xs !!? (i-1)
 
 main :: IO ()
 main = hspec spec
@@ -22,3 +29,17 @@ spec = do
       (Single (Product 2) 'a') +++ (Single (Product 3) 'b')
       `shouldBe`
       (Append (Product 6) (Single (Product 2) 'a') (Single (Product 3) 'b'))
+
+    it "should support indexing" $ do
+      let jl = Append (Size 3)
+                   (Append (Size 2)
+                     (Single (Size 1) "y")
+                     (Single (Size 1) "ea"))
+                   (Single (Size 1) "h")
+      let t = (\i -> (indexJ i jl) `shouldBe` (jlToList jl !!? i))
+
+      t 0
+      t 1
+      t 2
+      t 3
+      t 4
