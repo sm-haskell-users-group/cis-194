7f86e2257cefc5447eaaecd232f8d73c9b070bdf
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-10-03 12:50:20 -0700

    Complete Assignment 1

--- src/Cis194/Hw/Week1.hs
+++ src/Cis194/Hw/Week1.hs
@@ -5,22 +5,25 @@ module Cis194.Hw.Week1 where
 -------------
 
 lastDigit :: Integer -> Integer
-lastDigit _ = undefined
+lastDigit n = mod n 10
 
 dropLastDigit :: Integer -> Integer
-dropLastDigit _ = undefined
+dropLastDigit n = div n 10
 
 toDigits :: Integer -> [Integer]
-toDigits _ = undefined
+toDigits n | n < 1 = []
+           | otherwise = toDigits (dropLastDigit n) ++ [lastDigit n]
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther _ = undefined
+doubleEveryOther xs = snd $ foldr (\x (bit, acc) ->
+  ((if bit == 2 then 1 else 2), ((x * bit) : acc))) (1, []) xs
 
 sumDigits :: [Integer] -> Integer
-sumDigits _ = undefined
+sumDigits [] = 0
+sumDigits (x:xs) = sum (toDigits x) + sumDigits xs
 
 validate :: Integer -> Bool
-validate _ = undefined
+validate n = 0 == sumDigits (doubleEveryOther (toDigits n)) `mod` 10
 
 ---------------------
 -- Towers of Hanoi --
@@ -30,4 +33,6 @@ type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi _ _ _ _ = undefined
+hanoi 0 _ _ _    = []
+hanoi 1 p1 p2 _  = [(p1, p2)]
+hanoi n p1 p2 p3 = (hanoi (n-1) p1 p3 p2) ++ [(p1, p2)] ++ (hanoi (n-1) p3 p2 p1)
