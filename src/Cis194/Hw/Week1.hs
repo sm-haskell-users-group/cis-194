8e28fd8a562e9bc37187ddd1be45e95328a1826a
Andrew Emmons <andrew@giantmedia.com>
2014-10-09 09:31:20 -0700

    Hw 1 complete.

--- src/Cis194/Hw/Week1.hs
+++ src/Cis194/Hw/Week1.hs
@@ -5,22 +5,23 @@ module Cis194.Hw.Week1 where
 -------------
 
 lastDigit :: Integer -> Integer
-lastDigit _ = undefined
+lastDigit x = x `mod` 10
 
 dropLastDigit :: Integer -> Integer
-dropLastDigit _ = undefined
+dropLastDigit x = x `div` 10
 
 toDigits :: Integer -> [Integer]
-toDigits _ = undefined
+toDigits 0 = []
+toDigits x = (toDigits (dropLastDigit x)) ++ [lastDigit x]
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther _ = undefined
+doubleEveryOther xs = foldl (\acc (a,b) -> acc ++ [if a then b*2 else b]) [] (zip [i| j <- [1..], i <- [False,True]] xs)
 
 sumDigits :: [Integer] -> Integer
-sumDigits _ = undefined
+sumDigits xs = foldr (\x acc -> acc + if x < 10 then x else (sum . toDigits) x) 0 xs
 
 validate :: Integer -> Bool
-validate _ = undefined
+validate x = x `div` 10 == 0
 
 ---------------------
 -- Towers of Hanoi --
@@ -30,4 +31,10 @@ type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi _ _ _ _ = undefined
+hanoi numDisc p1 p2 p3
+    | numDisc == 1 = [(p1, p2)]
+    | numDisc > 1 = (hanoi (numDisc-1) p1 p3 p2) ++ (hanoi 1 p1 p2 p3) ++ (hanoi (numDisc-1) p3 p2 p1)
+    | otherwise = []
+
+hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
+hanoi4 _ _ _ _ _ = undefined
