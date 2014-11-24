657babd9d59f20d7dcb8585cdd873af9e3c3d529
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-11-24 18:13:17 +0000

    stub week 6

---
+++ src/Cis194/Hw/Week6.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE DeriveGeneric, OverloadedStrings #-}
+
+module HW06 where
+
+import Data.Aeson
+import Data.Monoid
+import GHC.Generics
+
+import qualified Data.ByteString.Lazy.Char8 as B
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
