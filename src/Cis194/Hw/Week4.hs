e63c03c2fca395807f3f79a6b9614301bcb2d73e
Devon Stewart <blast@hardchee.se>
2014-11-24 00:28:08 -0800

    w04: Adding stubs and BST

---
+++ src/Cis194/Hw/Week4.hs
@@ -0,0 +1,57 @@
+module Cis194.Hw.Week4 where
+
+import Cis194.Hw.BST
+
+impossible :: a
+impossible = undefined
+
+ex1 :: a -> b -> b
+ex1 _ _ = undefined
+
+ex2 :: a -> a -> a
+ex2 _ _ = undefined
+
+ex3 :: Int -> a -> a
+ex3 _ _ = undefined
+
+ex4 :: Bool -> a -> a -> a
+ex4 _ _ _ = undefined
+
+ex5 :: Bool -> Bool
+ex5 _ = undefined
+
+ex6 :: (a -> a) -> a
+ex6 _ = undefined
+
+ex7 :: (a -> a) -> a -> a
+ex7 _ _ = undefined
+
+ex8 :: [a] -> [a]
+ex8 _ = undefined
+
+ex9 :: (a -> b) -> [a] -> [b]
+ex9 _ _ = undefined
+
+ex10 :: Maybe a -> a
+ex10 _ = undefined
+
+ex11 :: a -> Maybe a
+ex11 _ = undefined
+
+ex12 :: Maybe a -> Maybe a
+ex12 _ = undefined
+
+insertBST :: (a -> a -> Ordering) -> a -> BST a -> BST a
+insertBST _ _ _ = undefined
+
+allCaps :: [String] -> Bool
+allCaps _ = undefined
+
+dropTrailingWhitespace :: String -> String
+dropTrailingWhitespace _ = undefined
+
+firstLetters :: [String] -> [Char]
+firstLetters _ = undefined
+
+asList :: [String] -> String
+asList _ = undefined
--- test/Cis194/Hw/Week4Spec.hs
+++ test/Cis194/Hw/Week4Spec.hs
@@ -36,3 +36,38 @@ spec = do
   describe "BST" $ do
     it "should be ordered" $ property $
       prop_ordered
+
+    it "should insert into empty trees" $ do
+      (insertBST compare 1 Leaf :: BST Int) `shouldBe` Node Leaf 1 Leaf
+
+    it "should insert into non-empty trees" $ do
+      (insertBST compare 2 (Node Leaf 1 Leaf) :: BST Int) `shouldBe` Node Leaf 1 (Node Leaf 2 Leaf)
+      (insertBST compare 1 (Node Leaf 2 Leaf) :: BST Int) `shouldBe` Node (Node Leaf 1 Leaf) 2 Leaf
+
+  describe "allCaps" $ do
+    it "should recognize empty lists as valid" $ do
+      allCaps [] `shouldBe` True
+
+    it "should recognize any number of valid strings" $ do
+      allCaps ["Abc", "Bce", "Cde"] `shouldBe` True
+
+    it "should recognize lowercase strings as invalid" $ do
+      allCaps ["Abc", "bce", "Cde"] `shouldBe` False
+
+  describe "dropTrailingWhitespace" $ do
+    it "should not modify strings without trailing spaces" $ do
+      dropTrailingWhitespace "abc" `shouldBe` "abc"
+
+    it "should drop trailing whitespace" $ do
+      dropTrailingWhitespace "abc        " `shouldBe` "abc"
+
+  describe "firstLetters" $ do
+    it "should extract first letters" $ do
+      firstLetters (words "Hey, how's it going?") `shouldBe` ['H', 'h', 'i', 'g']
+
+  describe "asList" $ do
+    it "should correctly format empty lists" $ do
+      asList [] `shouldBe` "[]"
+
+    it "should correctly format single-element lists" $ do
+      asList ["foo", "bar", "baz"] `shouldBe` "[foo,bar,baz]"
